6d4239825cd80ec3b26168e06039048c0bc0a23d
cjs0057 <cjs0057@auburn.edu>
2019-08-25 18:04:29 -0500

    Initial commit

--- src/main/java/Finder.java
+++ src/main/java/Finder.java
@@ -1,9 +1,45 @@
 public class Finder {
-    public int findMax(int[] intArray) {
 
+    public static void main(String[] args) {
+        int[] a1 = {2, 8, 7, 3, 4};
+        int[] a2 = {5, 9, 1, 7, 3};
+        int[] a3 = null;
+        int[] a4 = {2, 8, 8, 7, 3, 3, 4};
+
+        System.out.println(" max = " + findMax(a1) + " min = " + findMin(a1));
+        System.out.println(" max = " + findMax(a2) + " min = " + findMin(a2));
+        System.out.println(" max = " + findMax(a3) + " min = " + findMin(a3));
+        System.out.println(" max = " + findMax(a4) + " min = " + findMin(a4));
     }
 
-    public int findMin(int[] intArray) {
 
+    public static Integer findMax(int[] intArray) {
+        if ((intArray == null) || (intArray.length == 0)) {
+            return null;
+        }
+
+        int max = intArray[0];
+        for (int i = 1; i < intArray.length; i++) {
+            if (intArray[i] > max) {
+                max = intArray[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static Integer findMin(int[] intArray) {
+        if ((intArray == null) || (intArray.length == 0)) {
+            return null;
+        }
+
+        int min = intArray[0];
+        for (int i = 1; i < intArray.length; i++) {
+            if (intArray[i] < min) {
+                min = intArray[i];
+            }
+        }
+
+        return min;
     }
 }
